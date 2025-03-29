bc8fe9f3a261a7144ab5b1c2acde57953258f36f
Hiruni Ranasinghe <ranasinghehiruni14@gmail.com>
2025-03-29 23:41:05 +0530

    Find a student from the list by reg no

--- myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -24,6 +24,11 @@ public class AppController {
     Student bob = new Student("2020IT01", "Bob Marley", 23, "IT", 3.2);
     Student alice = new Student("2020IT02", "Alice Corner", 24, "IT", 3.6);
     Student john = new Student("2020IT03", "John Parker", 23, "IT", 3.3);
+    {
+    	students.add(bob);
+    	students.add(alice);
+    	students.add(john);
+    }
 	
 	@GetMapping("/student")
 	public Student getStudent(){
@@ -34,5 +39,15 @@ public class AppController {
 	public List<Student> getStudents(){
 	    
 	    return students;
+	}
+	@GetMapping("/student/{id}")
+	public Student getStudent(@PathVariable("id") String regno) {
+		for (Student student : students) {
+			if(student.getRegno().equals(regno)) {
+				return student;
+			}
+		}
+		
+		return null;
 	}
 }
